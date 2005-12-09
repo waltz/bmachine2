f282e0f2c227636105a2fd92f49be067fd8e9184
colin <colin@bce20531-a64c-4166-8316-56baf4a01729>
2005-12-09 23:18:34 +0000

    code for mod_rewrites and the new NotPublic flag
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@876 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine/index.php
+++ bmachine/index.php
@@ -10,7 +10,7 @@ require_once("theme.php");
 global $settings;
 
 if ($settings["DefaultChannel"] != "") {
-  header('Location: ' . get_base_url() . 'library.php?i=' . $settings["DefaultChannel"] );
+  header('Location: ' . channel_link($settings["DefaultChannel"]) );
   exit;
 }
 
@@ -24,11 +24,12 @@ front_header("Channel List");
 print("<div id=\"video_zone\">");
 
 foreach ($channels as $channel) {
+	if ( ! isset($channel["NotPublic"]) || ! $channel["NotPublic"] || valid_user() ) {
 ?>
 
 <div class="video_section">
-<h3 class="section_name"><a href="library.php?i=<?php echo $channel["ID"];  ?>"><?php echo $channel["Name"]; ?></a> 
-<a href="rss.php?i=<?php echo $channel["ID"]; ?>"><img src="images/rss_button.gif" alt="rss feed" border="0" /></a></h3>
+<h3 class="section_name"><a href="<?php echo channel_link($channel["ID"]); ?>"><?php echo $channel["Name"]; ?></a> 
+<a href="rss.php?i=<?php echo $channel["ID"]; ?>"><img src="images/rss_button.gif" alt="rss feed" width="20" height="9" style="border: 0" /></a></h3>
 
 <?php 
 $channel_files = $channel["Files"];
@@ -64,13 +65,14 @@ if ( count($channel_files) > 0 ) {
 
 <div class="spacer_left">&nbsp;</div>
 <div class="channel_more">
-<a href="library.php?i=<?php echo $channel["ID"]; ?>"><?php echo count($channel_files); ?> videos in this channel &gt;&gt;</a>
+<a href="<?php echo channel_link($channel["ID"]); ?>"><?php echo count($channel_files); ?> videos in this channel &gt;&gt;</a>
 </div>
 
 </div>
 
 <?php
-}
+	} // if
+} // foreach
 
 print("</div>");
 
