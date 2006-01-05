c5b61ee96633aa56e585f3e1e3b598cb525c497e
colin <colin@bce20531-a64c-4166-8316-56baf4a01729>
2006-01-05 13:17:27 +0000

    small formatting tweaks - fixed some tabs
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@948 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine/rss.php
+++ bmachine/rss.php
@@ -11,7 +11,7 @@ $channelID = $_GET["i"];
 $channels = $store->getAllChannels();
 if ( !isset($channels[$channelID]) ) {
   header("HTTP/1.0 404 Not Found");
-	exit;
+  exit;
 }
 
 $channel = $channels[$channelID];
@@ -19,10 +19,9 @@ $channel = $channels[$channelID];
 header('Content-Type: application/rss+xml; charset=utf-8');
 
 if ( isset($channel['RequireLogin']) && $channel['RequireLogin'] == 1 ) {
-	// fix bug #1229059 Can view passworded feeds without password
-	do_http_auth();
+  // fix bug #1229059 Can view passworded feeds without password
+  do_http_auth();
 }
 
 displayChannelRSS($channelID);
-
 ?>
