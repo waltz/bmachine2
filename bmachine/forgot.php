165d7d6bd67c1bb6d1a93b643acf022bbac50b2e
colin <colin@bce20531-a64c-4166-8316-56baf4a01729>
2006-03-28 16:34:12 +0000

    fixed potential xss issue, did some formatting
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1261 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine/forgot.php
+++ bmachine/forgot.php
@@ -6,71 +6,72 @@
  */
 
 require_once("include.php");
-require_once("theme.php");
 
 $found = false;
 
 if (isset($_POST["email"])) {
-	global $store;
+  $email = htmlentities($_POST["email"]);
 
-	$users = $store->getAllUsers();
+  global $store;
 
-	$username = "";
-	$newpass = "";
+  $users = $store->getAllUsers();
 
-	foreach ($users as $person) {
+  $username = "";
+  $newpass = "";
+	
+  foreach ($users as $person) {
 
-		if ($person["Email"] == $_POST["email"]) {
-			$username = $person["Name"];
-			$found = true;
-			break;
-		}
-	}
-
-	if ($found) {
-
-		// hash the user's name, password, and a secret key to generate a URL we can send to the user
-		$hash = urlencode(base64_encode( $username . "|" . $users[$username]['Hash'] ));
-		$url = get_base_url() . "forgot.php?hash=" . $hash;
-
-		if ( site_title() != "" ) {
-			$title = site_title() . "Broadcast Machine password";
-		}
-		else {
-			$title = "Broadcast Machine password";		
-		}
-
-		$result = @mail(
-			$_POST["email"],
-			$title,
-			"Click on this link to reset your password:\n\n$url"
-		);
-
-	}
+    if ($person["Email"] == $email) {
+      $username = $person["Name"];
+      $found = true;
+      break;
+    }
+  }
+  
+  if ($found) {
+    
+    // hash the user's name, password, and a secret key to generate a URL we can send to the user
+    $hash = urlencode(base64_encode( $username . "|" . $users[$username]['Hash'] ));
+    $url = get_base_url() . "forgot.php?hash=" . $hash;
+    
+    if ( site_title() != "" ) {
+      $title = site_title() . "Broadcast Machine password";
+    }
+    else {
+      $title = "Broadcast Machine password";		
+    }
+    
+    $result = @mail(
+		    $email,
+		    $title,
+		    "Click on this link to reset your password:\n\n$url"
+		    );
+    
+  }
 }
 
 
 if (isset($_POST["username"])) {
-
-	if (isset($_POST["pass1"])) {
-
-		if (($_POST["pass1"] == $_POST["pass2"]) && $_POST["pass1"] != "") {
-			$hash = hashpass($_POST["username"],$_POST["pass1"]);
-		}
-	}
-
-	// grab the user so we can figure out if they are admin or not
+  
+  if (isset($_POST["pass1"])) {
+    
+    if (($_POST["pass1"] == $_POST["pass2"]) && $_POST["pass1"] != "") {
+      $hash = hashpass($_POST["username"],$_POST["pass1"]);
+    }
+  }
+  
+  // grab the user so we can figure out if they are admin or not
   $user = $store->getUser($_POST["username"]);
-
-	$store->updateUser($_POST["username"],
-											$hash,
-											$_POST["email"],
-											isset($user["IsAdmin"]) && $user["IsAdmin"] == 1 ? true : false,
-											isset($user["IsPending"]) && $user["IsPending"] == 1 ? true : false );
-
-	login($_POST["username"], $_POST["pass1"], $msg);
-	header('Location: ' . get_base_url() . 'admin.php');
-	exit;
+  
+  $store->updateUser($_POST["username"],
+		     $hash,
+		     $email,
+		     isset($user["IsAdmin"]) && $user["IsAdmin"] == 1 ? true : false,
+		     isset($user["IsPending"]) && $user["IsPending"] == 1 ? true : false );
+  
+  login($_POST["username"], $_POST["pass1"], $msg);
+  header('Location: ' . get_base_url() . 'admin.php');
+  exit;
 }
 
 /**
@@ -130,26 +131,26 @@ if ( isset($_GET["hash"]) ) {
 	exit;
 }
 
-if (isset($_POST["email"])) {
-	if ($found) {
-		if ( $result == true ) {
+if (isset($email)) {
+  if ($found) {
+    if ( $result == true ) {
 ?>
 
-	<p>Your account information has been sent to <?php echo $_POST["email"]; ?>.</p>
+<p>Your account information has been sent to <?php echo $email; ?>.</p>
 <?php
-		}
-		else {
+    }
+    else {
 ?>
-	<p>BM can't send mail because of a problem with the mail configuration.  Please contact your server administrator.</p>
+	<p>Broadcast Machine can't send mail because of a problem with the mail configuration.  Please contact your server administrator.</p>
 <?php		
-		}
-	} 
-	else {
+    }
+  } 
+  else {
 ?>
-		<p>We were unable to find an account with the email address "<?php echo $_POST["email"]; ?>".  Please <a href="newuser.php">create a new account</a> or enter a different email address below.
+    <p>We were unable to find an account with the email address "<?php echo $email; ?>".  Please <a href="newuser.php">create a new account</a> or enter a different email address below.
 <?php
 
-	}
+  }
 }
 
 if (!$found) {
@@ -177,5 +178,5 @@ if (!$found) {
 </div> <!-- close login_box -->
 
 <?php
-	bm_footer();
+  bm_footer();
 ?>
