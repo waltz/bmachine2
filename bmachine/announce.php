18ebd6626f8d66cb4a7dcab3a96245278761600c
colin <colin@bce20531-a64c-4166-8316-56baf4a01729>
2006-03-30 01:11:16 +0000

    formatting
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1286 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine/announce.php
+++ bmachine/announce.php
@@ -12,22 +12,20 @@
 require_once("include.php");
 
 function er($txt) {
-	die('d14:failure reason' . strlen($txt) . ':' . $txt . 'e');
+  die('d14:failure reason' . strlen($txt) . ':' . $txt . 'e');
 }
 
 if((!isset($_GET['compact'])) || $_GET['compact'] != 1) {
-
-	er('This tracker requires new tracker protocol. Please use our Easy Downloader or check blogtorrent.com for updates.');
-
+  er('This tracker requires new tracker protocol. Please use our Easy Downloader or check blogtorrent.com for updates.');
 }
 
-$info_hash = isset($_GET['info_hash'])?$_GET['info_hash']:'';
+$info_hash = isset($_GET['info_hash']) ? $_GET['info_hash'] : '';
 
 if(strlen($info_hash) != 20) {
-	$info_hash = stripcslashes($info_hash);
+  $info_hash = stripcslashes($info_hash);
 }
 if(strlen($info_hash) != 20) {
-	er('Invalid info_hash');
+  er('Invalid info_hash');
 }
 
 $info_hash = bin2hex($info_hash);
@@ -35,12 +33,12 @@ $info_hash = bin2hex($info_hash);
 global $store;
 
 $return = $store->BTAnnounce(
-				$info_hash, isset($_GET['event'])?$_GET['event']:'',
-				$_SERVER['REMOTE_ADDR'],
-				isset($_GET['port'])?$_GET['port']:'',
-				isset($_GET['left'])?$_GET['left']:'',
-				isset($_GET['numwant'])?$_GET['numwant']:''
-			);
+			     $info_hash, isset($_GET['event'])?$_GET['event']:'',
+			     $_SERVER['REMOTE_ADDR'],
+			     isset($_GET['port'])?$_GET['port']:'',
+			     isset($_GET['left'])?$_GET['left']:'',
+			     isset($_GET['numwant'])?$_GET['numwant']:''
+			     );
 
 if (is_null($return)) {
   er($store->error);
