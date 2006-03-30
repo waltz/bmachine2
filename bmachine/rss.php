ded1d0addf6736946813b1945edbffe87aab9aa4
colin <colin@bce20531-a64c-4166-8316-56baf4a01729>
2006-03-30 17:08:54 +0000

    added mod_rewrite handling - if it's not working for some reason we parse the URL to figure out our params
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1292 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine/rss.php
+++ bmachine/rss.php
@@ -6,7 +6,16 @@
 
 require_once("include.php");
 
+// processing here in case our rewrite rules choke
+if ( !isset($_GET["i"]) ) {
+	$params = split("/", $_SERVER['REQUEST_URI']);
+	if ( count($params) == 4 ) {
+		$_GET["i"] = $params[3];
+	}
+}
+
 $channelID = $_GET["i"];
+$forceLoad = isset($_GET["force"]) ? true : false;
 
 $channels = $store->getAllChannels();
 if ( !isset($channels[$channelID]) ) {
@@ -18,10 +27,16 @@ if ( !isset($channels[$channelID]) ) {
 $channel = $channels[$channelID];
 
 header('Content-Type: application/rss+xml; charset=utf-8');
+header('Content-Disposition: inline; filename="' . $channelID . '.rss"');
+
 
 if ( isset($channel['RequireLogin']) && $channel['RequireLogin'] == 1 ) {
   // fix bug #1229059 Can view passworded feeds without password
   do_http_auth();
+}
+
+if ( $forceLoad == true ) {
+  makeChannelRSS($channelID, false);
 }
 
 displayChannelRSS($channelID);
