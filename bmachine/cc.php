4cd7098af40c8d03e2e00de53810411b4048c951
colin <colin@bce20531-a64c-4166-8316-56baf4a01729>
2006-04-17 23:39:05 +0000

    rename license url element to new scheme
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1374 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine/cc.php
+++ bmachine/cc.php
@@ -11,7 +11,7 @@
 
 <script lanaguage="Javascript">
 	frm = window.opener.document.getElementById('post');
-	frm.post_license_url.value = '<?php echo $license_url; ?>';
+	frm.LicenseURL.value = '<?php echo $license_url; ?>';
 	frm.post_license_name.value = '<?php echo $license_name; ?>';
 	self.close();
 </script>
