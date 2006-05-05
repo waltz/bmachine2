5a05fdfa54eebc91447290d31cf5f05828aa1633
colin <colin@bce20531-a64c-4166-8316-56baf4a01729>
2006-05-05 16:20:17 +0000

    commited some code for specifying IP address, but i didn't mean to, it's not for primetime yet
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1532 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine/announce.php
+++ bmachine/announce.php
@@ -8,9 +8,11 @@
  * @package Broadcast Machine
  */
 
-
 require_once("include.php");
 
+/**
+ * die and send an error message back to the bittorrent client
+ */
 function er($txt) {
   die('d14:failure reason' . strlen($txt) . ':' . $txt . 'e');
 }
@@ -23,21 +25,35 @@ $info_hash = isset($_GET['info_hash']) ? $_GET['info_hash'] : '';
 
 if(strlen($info_hash) != 20) {
   $info_hash = stripcslashes($info_hash);
-}
-if(strlen($info_hash) != 20) {
-  er('Invalid info_hash');
-}
 
+  if(strlen($info_hash) != 20) {
+    er('Invalid info_hash');
+  }
+}
 $info_hash = bin2hex($info_hash);
+
+$ip_address = $_SERVER['REMOTE_ADDR'];
+/*
+if ( isset($_GET["ip"]) && $_GET["ip"] != "" ) {
+
+  // make sure the incoming IP is valid
+  $num = "(25[0-5]|2[0-4]\d|[01]?\d\d|\d)";
+  if ( preg_match("/^$num\\.$num\\.$num\\.$num$/", $_GET["ip"],$match ) ) {
+    $ip_address = $_GET["ip"];
+    debug_message("using specified IP address $ip_address");
+  }
+}*/
 
 global $store;
 
+
 $return = $store->BTAnnounce(
-			     $info_hash, isset($_GET['event'])?$_GET['event']:'',
-			     $_SERVER['REMOTE_ADDR'],
-			     isset($_GET['port'])?$_GET['port']:'',
-			     isset($_GET['left'])?$_GET['left']:'',
-			     isset($_GET['numwant'])?$_GET['numwant']:''
+			     $info_hash,                                     // hash of torrent
+			     isset($_GET['event']) ? $_GET['event'] : '',    // event being announced
+			     $ip_address,                                    // IP of remote client
+			     isset($_GET['port']) ? $_GET['port'] : '',      // port remote client is using
+			     isset($_GET['left']) ? $_GET['left'] : '',      // amount left in download (if 0, this is a seeder)
+			     isset($_GET['numwant']) ? $_GET['numwant'] : '' // number of client IPs requested
 			     );
 
 if (is_null($return)) {
