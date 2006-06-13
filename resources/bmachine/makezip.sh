a1b862b04f89330986559fb6f0994fec9f325d22
nassar <nassar@bce20531-a64c-4166-8316-56baf4a01729>
2006-06-13 17:11:17 +0000

    Added support for automatically downloading the binary kit from svn
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2010 bce20531-a64c-4166-8316-56baf4a01729

--- resources/bmachine/makezip.sh
+++ resources/bmachine/makezip.sh
@@ -7,6 +7,7 @@
 rm bm.zip
 rm -rf bm
 svn co https://svn.participatoryculture.org/svn/dtv/trunk/bmachine ./bm
+svn co https://svn.participatoryculture.org/svn/dtv/trunk/bmachine-binary-kit ./binaries
 cp ./binaries/* ./bm/
 find ./bm -name '.svn' -exec rm -rf {} \;
 find ./bm -name 'CVS' -exec rm -rf {} \;
