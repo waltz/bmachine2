021fb0f57180374b6d458162319b72caac73cb42
oppy <oppy@bce20531-a64c-4166-8316-56baf4a01729>
2006-07-14 21:33:23 +0000

    added some missing files. Fixed a header for absolute links (the forums need them)
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2344 bce20531-a64c-4166-8316-56baf4a01729

---
+++ web/getdemocracy/include/wphead.php
@@ -0,0 +1,12 @@
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<link href="/stylesheets/main.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/ico" href="http://getdemocracy.com/favicon.ico" />
+
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="http://getdemocracy.com/news/feed" />
+
+<script src="/js/prototype.js" type="text/javascript"></script>
+<script src="/js/scriptaculous.js" type="text/javascript"></script>
+<script src="/js/effects.js" type="text/javascript"></script>
+<script src="/js/mailinglist.js" type="text/javascript"></script>
+
---
+++ web/getdemocracy/include/wpheader.php
@@ -0,0 +1,20 @@
+<div id="header">
+	<div id="header-top">
+			<h1><a href="http://getdemocracy.com">Democracy - Internet TV Platform - Free and Open Source</a></h1>
+			<ul id="nav-main">
+				<li><a href="http://getdemocracy.com/downloads">Downloads</a></li>
+				<li><a href="http://forum.getdemocracy.com">Forums</a></li>
+				<li><a href="http://getdemocracy.com/donate">Donate</a></li>
+				<li><a href="http://getdemocracy.com/help">Help</a></li>
+				<li><a href="http://getdemocracy.com/about">About</a></li>
+				<li><a href="http://getdemocracy.com/news">Blog</a><a href="http://getdemocracy.com/news/feed/"><img src='http://getdemocracy.com/images/feed-white.gif'  /></a></li>
+			</ul>
+	</div>
+			<ul id="nav-sub">
+				<li id="nav-watch"><a href="http://getdemocracy.com/watch">Watch TV - Get Democracy player and explore internet TV</a></li>
+				<li id="nav-make"><a href="http://getdemocracy.com/make">Make TV - Turn your videos into an Internet TV channel</a></li>
+				<li id="nav-code"><a href="http://getdemocracy.com/code">Code - Get under the hood. 100% open-source, GPL</a></li>
+			</ul>
+
+</div>
+
