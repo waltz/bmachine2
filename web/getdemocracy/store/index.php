cf7a6a5d90af078a36244d74ab78779ced9b6fbd
clahey <clahey@bce20531-a64c-4166-8316-56baf4a01729>
2006-07-18 01:38:31 +0000

    Fixed layout of store a little.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2390 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/store/index.php
+++ web/getdemocracy/store/index.php
@@ -1,6 +1,8 @@
 <?php include("../include/start.php"); ?>
 
-<iframe src="http://www.vgkids.com/cgi-bin/ccp51/cp-app.cgi?pg=cat&ref=pcf&affl=pcf" style="width:99%; height:550px; border:none;">
+<div id="content">
+<iframe src="http://www.vgkids.com/cgi-bin/ccp51/cp-app.cgi?pg=cat&ref=pcf&affl=pcf" style="width: 100%; height: 550px; border:none;">
 </iframe>
+</div>
 
 <?php include("../include/end.php"); ?>
