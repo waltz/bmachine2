890df94b3bb71addd10d9fa10ec3106fb9bf3444
oppy <oppy@bce20531-a64c-4166-8316-56baf4a01729>
2006-07-24 20:18:05 +0000

    Super minor change to forums/wordpress header (removed border on image)
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2461 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/include/wpheader.php
+++ web/getdemocracy/include/wpheader.php
@@ -7,7 +7,9 @@
 				<li><a href="http://getdemocracy.com/donate">Donate</a></li>
 				<li><a href="http://getdemocracy.com/help">Help</a></li>
 				<li><a href="http://getdemocracy.com/about">About</a></li>
-				<li><a href="http://getdemocracy.com/news">Blog</a><a href="http://getdemocracy.com/news/feed/"><img src='http://getdemocracy.com/images/feed-white.gif'  /></a></li>
+				<li><a href="http://getdemocracy.com/news">Blog</a>
+					<a href="http://getdemocracy.com/news/feed/">
+					<img src='http://getdemocracy.com/images/feed-white.gif' border="0"/></a></li>
 			</ul>
 	</div>
 			<ul id="nav-sub">
