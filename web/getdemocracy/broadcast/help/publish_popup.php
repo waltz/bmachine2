09a4e17122f43186a73c7681cbc6ac06deddcc17
oppy <oppy@bce20531-a64c-4166-8316-56baf4a01729>
2006-07-25 23:31:46 +0000

    Added BM help section, which we missed when updating to the new site
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2480 bce20531-a64c-4166-8316-56baf4a01729

---
+++ web/getdemocracy/broadcast/help/publish_popup.php
@@ -0,0 +1,51 @@
+<?php 
+include 'popup_head.html';
+?>
+
+<div class="help_pop">
+<h2>Help: Publishing a video</h2>
+
+<h3>Posting a File</h3>
+
+<p>
+You can post a file either by sharing it from your computer ("Post a Torrent"), by uploading it directly to your server ("Upload a File"), or simply by linking to the URL of a file that is already available on the web ("Specify URL").
+</p>
+
+<h3>Posting with BitTorrent</h3>
+
+
+<p>
+To post a file from your computer using BitTorrent, click "Post a Torrent" on the publish screen.  If you've never done this before, you'll need to download Broadcast Machine Helper first.  Links to the download are located below the 'Post a Torrent' button.</p>
+
+<p>
+Once you have Broadcast Machine Helper installed, when you click 'Post a Torrent' you will be asked to choose a file to upload.  Once you choose a file, you can return to the publish screen and continue adding information about the file.  <strong>For your files to be available, you must keep the helper application open and sharing those files.</strong>
+</p>
+
+<p>
+If you're interested in sharing the file from your server using BitTorrent, click on the 'Settings' tab in Broadcast Machine to learn about server-sharing.
+For more about posting files and using BM Helper, see the <a target="_blank" href="http://www.participatoryculture.org/broadcast/help">BM FAQ</a>.
+</p>
+
+<h3>Entering Basic Info</h3>
+
+<p>
+Once you've picked a video to publish, you should enter some basic information that will tell viewers about the video.  Most of these are self, explanatory, but here are a few notes:
+</p>
+
+<p>
+<strong>Publish to These Channels:</strong> to have the video appear on a Channel, you must select it. You can publish a video to more than one channel at a time.
+</p>
+
+<p><strong>Thumbnail:</strong> an image from the video that is representative or enticing to potential viewers. Images should generally be JPG, GIF, or PNG format. Bigger images are better (up to 200k) and Broadcast Machine will resize them as needed.</p>
+
+<p><strong>Creative Commons license:</strong> If you are the copyright holder for the file, you can choose to publish it with a Creative Commons license. These licenses will allow other people to use your work in ways that you specify, so that they can share and build on your creativity.</p>
+
+<h3>Optional: Additional Info</h3>
+
+<p>The more information you include about your video, the more ways people will be able to find it by searching, sorting, and filtering. For example, adding information about where the video was made will eventually let people discover interesting media being made in their own backyard.</p>
+
+</div>
+
+<?php 
+include 'popup_footer.html';
+?>
---
+++ web/getdemocracy/broadcast/help/torrent_posting.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><head><title>Broadcast Machine</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="pub_css.css"/>
+
+
+<SCRIPT LANGUAGE="JavaScript">
+<!-- Idea by:  Nic Wolfe (Nic@TimelapseProductions.com) -->
+<!-- Web URL:  http://fineline.xs.mw -->
+
+<!-- This script and many more are available free online at -->
+<!-- The JavaScript Source!! http://javascript.internet.com -->
+
+<!-- Begin
+function popUp(URL) {
+day = new Date();
+id = day.getTime();
+eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=0,width=450,height=630');");
+}
+// End -->
+</script>
+
+
+</head>
+
+<body>
+
+<div id="head"><h1></h1></div>
+<div class="wrap">
+
+<br />
+<div class="page_name">
+<h2>Torrent Troubleshooting</h2>
+</div>
+
+<p>Posting a torrent file can be tricky at first, but it will reduce your bandwidth costs to virtually zero.  This is very important for most sites that host high quality videos.  Broadcast Machine combines a number of technologies that make torrent posting easier than ever before-- and very managable for most sites.</p>
+
+<p><strong>The most common cause of problems with torrent files is that the BM Helper window must remain open for the torrent file to be available on your site.</strong>  If you use server-sharing, you can avoid this problem, but it will require using some bandwidth and disk space.  See below.</p>
+
+<h3>I clicked "Post a Torrent" but nothing happened.</h3>
+
+<p>
+To post a torrent, you'll need to first install Broadcast Machine helper.  There's a link to download the helper for mac or windows on the "Publish" tab, in the file posting box.  Once you download it, you'll need to install it.  
+</p>
+
+<p><strong>On Windows:</strong> run the BM Helper file that you download.  It will ask you to quit your browsers so that it can install the plugin which simplifies uploading torrents.
+</p>
+
+<p><strong>On Mac:</strong> you need to drag 'BM Helper' into your applications folder and run the program once, after quiting your web browser.  This will install the plugin that helps simplify torrent posting. 
+</p>
+
+<p>After you've installed BM Helper, when you click "Post a Torrent" you should be prompted to choose a file that you'd like to publish.</p>
+
+
+<h3>I shared a file, but it won't download.</h3>
+
+<p>Testing whether a torrent is working can be harder than getting it to work.  Paradoxically, just because you aren't able to download the file you posted, doesn't mean that it's not working properly (ok, that's not actually a paradox, but it is odd).  Here's some reasons why you might not be able to download a file that you posted:</p>
+
+<p>1. <em>You closed the BM Helper window that was sharing the file.</em>  If you closed the window in BM Helper that opened when you posted the file, or if you quit BM Helper, restarted your computer, turned off your computer, etc-- the file will no longer be shared from your computer.  To start sharing the file again, you'll need to go into the 'files' tab and click 'download' on the file that you want to share.  BM Helper may ask you where you want to save the file-- instead of picking a place to save the file, go into the folder where the file is located and select that folder.  BM Helper may act like it is going to overwrite the file, but in fact it will recognize that you have the complete file and resume sharing it.</p>
+
+<p>2. <em>You can't download a torrent from the same computer that you're sharing it.</em>  This just won't work correctly in most situations.</p>
+
+<p>3. <em>You may not be able to download a torrent from a computer that's on the network / router / broadband connection as the computer that is sharing the file.</em>  Ask a friend somewhere else to give it a try.</p>
+
+<p>4. <em>If the computer sharing the file and the computer attempting to download the file are both on internet connections that have firewalls (virtually every common broadband router has a firewall) then you might not be able to transfer the file.</em>  This makes testing a torrent difficult, but in practice doesn't prevent people from getting the file.  This is because even if a single computer that's attempting to download the file is on a connection with no firewall or has forwarded the ports on their router, they will be able to help the computer that are behind a firewall work around it. </p>
+
+<p>5. <em>It might just be slow to get started.</em>  Sometimes torrent downloads can take 5 minutes or more to get started.  This might make it appear that the download is not working.  Be patient.</p>
+
+<p>Once you succesfully share a few files, you won't need to test every one to be confident that they are working.</p>
+
+<strong><a href="index.php"><< Back to Main Help</a></strong>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
