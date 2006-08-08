4702eb6930416d786e6082f6c2cbda5c6f1304ea
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-08-08 17:23:54 +0000

    download page stuff
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2616 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/downloads/index.php
+++ web/getdemocracy/downloads/index.php
@@ -123,7 +123,7 @@ include "../include/start.php";
 				<a href="<?= $base ?>/make">Make TV page >></a></p>
 
 				<h6>Translators</h6>
-				<p>Democracy Player supports multiple languages-- we need your help!  <a href="https://launchpad.net/products/democracy/trunk/+pots/democracyplayer">Join the translation effort</a>.
+				<p>Democracy Player supports multiple languages-- we need your help! <br /><a href="https://launchpad.net/products/democracy/trunk/+pots/democracyplayer">Join the translation effort.</a>
 				</p>
 
 				</div>
