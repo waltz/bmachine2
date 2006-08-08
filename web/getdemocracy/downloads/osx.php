bb5fc58d556648f3046840d6368094c58864ac2b
oppy <oppy@bce20531-a64c-4166-8316-56baf4a01729>
2006-08-08 08:42:10 +0000

    Took out the Mac triage pages, we now have a UB.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2609 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/downloads/osx.php
+++ web/getdemocracy/downloads/osx.php
@@ -1,19 +1,49 @@
-<?php include("../include/start.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<title>Democracy: Internet TV</title>
+
+<?php include("../include/wphead.php"); ?>
+
+<meta http-equiv="Refresh" content="2;
+URL=http://ftp.osuosl.org/pub/pculture.org/democracy/osx/Democracy-0.8.5.3.dmg">
+
+
+</head>
+
+<body>
+
+<?php include("../include/wpheader.php"); ?>
 
 <div id="content">
 
 
 	<!--CONTENT BLOCK-->
+  <div class="content-1col">
 
+			<div class="big" style="margin:20px 0px 15px 8px; text-align:center; color: #444;"><strong>Democracy Player for OS X is downloading...</strong>
 
-			<div class="big" style="margin:20px 0px 15px 8px; text-align:center; color: #444;"><h3>Choose your download:</h3>
-				<p style="text-align:center; font-size: 20px;"><a href="osx-ppc.php">Democracy for PPC Mac</a>&nbsp;&nbsp;-&nbsp;&nbsp;<a href="osx-intel.php">Democracy for Intel Mac</a></p>
-				
-				<p><strong>Not sure which one to pick?</strong>  If you bought your Mac before February 2006, you almost certainly have a PPC Mac.  If you bought a Mac recently and have an iMac, MacBook or MacBook Pro or a MacMini, you probably have an Intel Mac.  Hopefully, we'll offer a 'Universal Binary' soon and won't need to offer separate versions.</p>
+						<p style="text-align:center; font-size: 12px;">
+						To watch Windows Media videos in Democracy, <a href="http://www.microsoft.com/windows/windowsmedia/player/wmcomponents.mspx">get this free plugin.</a><br /><br />
+						If your download didn't start automatically, <a href="http://ftp.osuosl.org/pub/pculture.org/democracy/osx/Democracy-0.8.5.3.dmg">click here</a>.</p>
+
 			</div>
 
 
 
+
+
+			<div class="post-download-box">
+
+
+			<?php include("../downloads/message.php"); ?>
+
+
+			</div>
+
+
+  </div>
   <!--/CONTENT BLOCK-->
 
 	<!--FOOTER-->
