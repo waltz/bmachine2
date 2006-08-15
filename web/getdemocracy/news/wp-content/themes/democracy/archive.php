a38f5d5d5d18872d206cbd7533c91170326d0af5
josh <josh@bce20531-a64c-4166-8316-56baf4a01729>
2006-08-15 21:32:04 +0000

    archives
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2731 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/news/wp-content/themes/democracy/archive.php
+++ web/getdemocracy/news/wp-content/themes/democracy/archive.php
@@ -14,12 +14,12 @@
 
 					
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-					<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h1>
-					<h3><?php the_time('l, F jS, Y') ?> <span style="font-weight:normal;">at</span> <?php the_time() ?> <span style="font-weight:normal;">by</span> <?php the_author() ?></h3>
-					
+					<div class="dateblock"><div class="datetop"><?php the_time('M') ?></div><div class="datebottom"><?php the_time('d') ?></div></div>
+					<h2 class="posttitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+					<div class="byaux">by</span> <?php the_author() ?></div>
+					<div class="postline">
 					<?php the_content(__('(more...)')); ?>
-					
+					</div>
 					<?php endwhile; else: ?>
 					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 					<?php endif; ?>
