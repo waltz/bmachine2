f76d4fad441215db03828234ab83bde20b29d2a9
josh <josh@bce20531-a64c-4166-8316-56baf4a01729>
2006-08-15 21:33:42 +0000

    archives
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2732 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/news/wp-content/themes/democracy/archive.php
+++ web/getdemocracy/news/wp-content/themes/democracy/archive.php
@@ -17,7 +17,7 @@
 					<div class="dateblock"><div class="datetop"><?php the_time('M') ?></div><div class="datebottom"><?php the_time('d') ?></div></div>
 					<h2 class="posttitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 					<div class="byaux">by</span> <?php the_author() ?></div>
-					<div class="postline">
+					<div class="commentline">
 					<?php the_content(__('(more...)')); ?>
 					</div>
 					<?php endwhile; else: ?>
