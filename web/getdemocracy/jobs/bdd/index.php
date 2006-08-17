163bfb440b7b4940b1bf16064ffc1078ee121e5e
oppy <oppy@bce20531-a64c-4166-8316-56baf4a01729>
2006-08-17 23:50:56 +0000

    Early job posting so we dont have a dead link
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2778 bce20531-a64c-4166-8316-56baf4a01729

---
+++ web/getdemocracy/jobs/bdd/index.php
@@ -0,0 +1,27 @@
+<?php include("../include/start.php"); ?>
+<!--CONTENT BLOCK--> <div id="content">
+
+<div class="left">
+
+<div style="margin-left:5px;">
+
+<h3>Participatory Culture Foundation</h3>
+<h3>Director of Business Development</h3>
+
+
+
+
+<h3 style="margin-top:50px;">Who We Are</h3>
+
+<p>For more about the Participatory Culture Foundation:<br /><br />
+<strong><a href="http://participatoryculture.org/">PCF Homepage</a><br />
+<a href="<?= $base ?>/news/">News and Blog</a><br />
+</strong>
+
+
+</div>
+</div>
+
+</div> <!--/CONTENT BLOCK-->
+
+<!--FOOTER--> <?php include("../include/end.php"); ?> <!--/FOOTER-->
