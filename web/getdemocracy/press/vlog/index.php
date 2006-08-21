d2afce9459021c08c55bcab373356445444fbcb8
oppy <oppy@bce20531-a64c-4166-8316-56baf4a01729>
2006-08-21 18:27:28 +0000

    New press release
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2800 bce20531-a64c-4166-8316-56baf4a01729

---
+++ web/getdemocracy/press/vlog/index.php
@@ -0,0 +1,38 @@
+<?php include("../../include/start.php"); ?>
+<!--CONTENT BLOCK--> <div id="content">
+
+<div class="left">
+
+<div style="margin-left:5px; padding-top: 10px;">
+
+<p><b>RELEASE DATE August 16, 2006<br />          					
+<br />
+CONTACT: Dean Jansen<br />
+dean@pculture.org - mobile:  857-928-4867<br />
+Participatory Culture Foundation</b></p>
+
+<h3>Participatory Culture Foundation and Knight Foundation partner to Teach Video News Blogging</h3>
+
+<p>If you want to know how to publish video on the Internet, the Participatory Culture Foundation is creating a step-by-step tutorial with a $50,000 grant from the John S. and James L. Knight Foundation.</p>
+
+<p>The free tutorials will teach individuals to shoot and publish news video to the web. The lessons will feature tools that are free and open to everyone, such as the Democracy Player, a comprehensive, open-source Internet TV system developed by the Participatory Culture Foundation (PCF).</p>
+
+<p>People will learn how to select the right equipment, shoot news video, prepare that video for the Internet, and then publish it to thousands of viewers. The free Democracy Player has been downloaded more than 400,000 times.</p>
+
+<p>"News video is rapidly moving online, and these tutorials will make it easier for people to create their own news on Internet TV," said Dean Jansen of the Participatory Culture Foundation. "We envision these tutorials being used by people from all backgrounds to create video about locally relevant community news, whether it is severe potholes on municipal roads or the need for a community daycare center."</p>
+
+<p>"This grant extends Knight Foundation’s support for citizen journalism by demystifying video blogging of news events," said Gary Kebbel, Knight Foundation journalism program officer.</p>
+
+<p>Knight Foundation continues to increase its support of online journalism and citizen reporting to help communities identify their problems and tap their potential to seek solutions. This fall, the foundation plans to create a competition for proposals that use news and information in new ways to improve community life. "We want ideas that use electronic media to help improve the lives of individuals where they live and work," Kebbel said. </p>
+
+<p><b>PCF’s Democracy Video Player</b> builds on cutting-edge RSS feeds, the Firefox browser, and BitTorrent syndication technology to empower anyone to watch, share, broadcast and download video over the Internet. The Democracy Player enables higher digital resolution, full-screen video playback, continuous non-buffered play and an open standards environment free of adware or spyware -- a much more TV-like experience than traditional web video. (Download Democracy free at <b><a href="http://www.getdemocracy.com/">www.getdemocracy.com</a></b>.)</b>
+
+<p>The John S. and James L. Knight Foundation promotes excellence in journalism worldwide and invests in the vitality of communities where the Knight brothers owned newspapers. To see and hear stories of Knight’s transformational funding, visit <a href="http://www.knightfdn.org/annual/">www.knightfdn.org/annual/</a>.</p>
+
+
+</div>
+</div>
+
+</div> <!--/CONTENT BLOCK-->
+
+<!--FOOTER--> <?php include("../../include/end.php"); ?> <!--/FOOTER-->
