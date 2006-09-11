703b88a0aea5450cb04a640e567b732f98879bd8
oppy <oppy@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-11 14:08:41 +0000

    Feature comparison
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3072 bce20531-a64c-4166-8316-56baf4a01729

---
+++ web/getdemocracy/articles/features/index.php
@@ -0,0 +1,194 @@
+<?php
+include ("../../include/start.php");
+?>
+
+	<div id="sub-main">
+		<h2>Democracy Feature List Comparison</h2>
+		<p class="subhead">You name it, Democracy's got it. See for yourself - compare Democracy's features to the other major video players, iTunes and Windows Media Player: </p><br/>
+		<div id="featurelist"><table border="0">
+			<!--The software-->
+			<tr><td class="top">&nbsp;</td>
+			<td  class="top" colspan="3"><img src="images/democracy.gif" /></td>
+			<td  class="top" colspan="3"><img src="images/itunes.gif" /></td>	
+			<td  class="top" colspan="3"><img src="images/winmedia.gif" /></td>
+			</tr>
+
+			<!--The platforms-->
+			<tr><td class="head">Features:</td>
+                     <td class="head"><img src="images/winvista.png" /></td>
+                     <td class="head"><img src="images/macosx.png" /></td>
+			<td class="head"><img src="images/linux.png" /></td>
+                     <td class="head"><img src="images/winvista.png" /></td>
+                     <td class="head"><img src="images/macosx.png" /></td>
+			<td class="head"><img src="images/linux.png" /></td>
+                     <td class="head"><img src="images/winvista.png" /></td>
+                     <td class="head"><img src="images/macosx.png" /></td>
+			<td class="head"><img src="images/linux.png" /></td>
+                        </tr>
+
+			<!--Feature list-->
+			<tr><td>Video Content</td>
+                        <td class="yes">Free</td>
+                        <td class="yes">Free</td>
+                        <td class="yes">Free</td>
+                        <td class="partial">iTunes Store</td>
+                        <td class="partial">iTunes Store</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			<tr><td>Fullscreen</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="partial">No Controls</td>
+                        <td class="partial">No Controls</td>
+                        <td class="no">No</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			<tr><td>Playlists</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			<tr><td>BitTorrent</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			<tr><td>Google/Youtube/Yahoo search</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			<tr><td>Open Source</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			
+
+			<!--Format list-->
+
+			<tr><td class="head">Supported Formats:</td>
+                     <td class="head"><img src="images/winvista.png" /></td>
+                     <td class="head"><img src="images/macosx.png" /></td>
+			<td class="head"><img src="images/linux.png" /></td>
+                     <td class="head"><img src="images/winvista.png" /></td>
+                     <td class="head"><img src="images/macosx.png" /></td>
+			<td class="head"><img src="images/linux.png" /></td>
+                     <td class="head"><img src="images/winvista.png" /></td>
+                     <td class="head"><img src="images/macosx.png" /></td>
+			<td class="head"><img src="images/linux.png" /></td>
+                    </tr>
+
+			<tr><td>MPEG</td>
+			   <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+			</tr>
+
+                        <tr><td>ASF/WMV/WMA</td>
+                        <td class="yes">Yes</td>
+                        <td class="partial">w/Plugin</td>
+                        <td class="yes">Yes</td>
+                        <td class="partial">w/Plugin</td>
+                        <td class="partial">w/Plugin</td>
+                        <td class="no">No</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+			</tr>
+
+			<tr><td>MP4/MOV/3GP</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			<tr><td>AVI (Xvid/DivX/etc)</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			<tr><td>OGG</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="partial">w/Plugin</td>
+                        <td class="partial">w/Plugin</td>
+                        <td class="no">No</td>
+                        <td class="partial">w/Plugin</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+			<tr><td>Flash (FLV)</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="yes">Yes</td>
+                        <td class="no">No</td>
+                        <td class="no">No</td>
+                        </tr>
+
+		</table>
+		</div>
+	</div>
+
+<?php include ("../../include/end.php"); ?>
