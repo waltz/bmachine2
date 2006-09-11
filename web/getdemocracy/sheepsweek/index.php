5d5d06136b0161413447b9d78b6a48bb4dc695bb
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-11 16:23:51 +0000

    sheeps tweaks
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3084 bce20531-a64c-4166-8316-56baf4a01729

---
+++ web/getdemocracy/sheepsweek/index.php
@@ -0,0 +1,51 @@
+<?php include "../include/start.php"; ?>
+		
+
+		
+		<div style="position: relative; padding-top: 0;" id="content">
+
+
+		<img src="sheep.png" alt="sheeps week" />
+
+		
+		
+		<p style="color: #666; padding-bottom: 0px;">
+		This week we're taking internet TV to the next level.	Each day we'll announce something new: a major new version of <a style="color: #666; font-weight: normal;" href="http://www.getdemocracy.com">Democracy Player</a>, new websites, new articles.  Every project is designed to either support internet video creators or to give viewers the best internet tv experience ever.  Why 'sheeps week'?  Because we have a weeks worth of stuff and this was the best pun on '<a style="color: #666; font-weight: normal;" href="http://www.slate.com/id/2095577/">sweeps week</a>' that we could think of.
+</p>		
+		<p style="text-align: right; "><a style="text-decoration: none;" href="http://getdemocracy.com/news/feed/"><img src="http://getdemocracy.com/images/feed-white.gif"></a> <a style="color: #555; font-weight: normal;" href="http://boomdock.local/~nicholaspreville/getdemocracy/news/feed/">RSS Feed</a>	
+		</p>
+		
+
+		
+		
+		<div style="position: relative; padding: 10px 0 20px 0; border-top: 1px solid #ddd;">
+		<img style="float: left;" src="sheeps-monday.png" alt="monday" />
+		
+				
+		<h2 style="padding: 15px 0 0 300px; margin: 0; font-size: 23px;"><a href="http://www.getdemocracy.com">Democracy Player 0.9 Released!</a></h2>
+		<p style="padding: 5px 0 0 300px; margin: 0;">
+		A major update to the best video RSS player in the world.  New features, new polish.
+		</p>
+			
+		</div>
+
+
+<div style="clear: both; position: relative; padding: 10px 0 20px 0; border-top: 1px solid #ddd;">
+	
+		<img style="float: left;" src="sheeps-tuesday.png" alt="monday" />
+		
+				
+		<h2 style="padding: 15px 0 0 300px; margin: 0; font-size: 23px;">...</h2>
+		<p style="padding: 5px 0 0 300px; margin: 0;">
+		Check back around 1 PM EST on Tuesday.<br />&nbsp;
+		</p>
+			
+		</div>
+
+<div style="clear: both; position: relative; padding: 10px 0 20px 0; border-top: 1px solid #ddd;">
+&nbsp;
+</div>
+
+<br /><br /><br /><br /><br />
+
+<?php include "../include/end.php"; ?>
