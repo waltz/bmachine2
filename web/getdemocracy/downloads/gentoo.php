2d5f461824aa3b86ce9368e6ca2753709aa8456b
oppy <oppy@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-13 05:49:43 +0000

    fixed typo
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3141 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/downloads/gentoo.php
+++ web/getdemocracy/downloads/gentoo.php
@@ -7,7 +7,7 @@
 			<!--<div class="big" style="margin:20px 0px 15px 8px; text-align:center; color:
 #444;"><strong>Democracy Player is downloading...</strong>-->
 
-		<p style="text-align:center; font-size: 14px;">Democracy Player is part of Project Sunrise.  You can get the latest version of democracy Democracy using these commands in a terminal window:</p>
+		<p style="text-align:center; font-size: 14px;">Democracy Player is part of Project Sunrise.  You can get the latest version of Democracy using these commands in a terminal window:</p>
 <pre style="text-align: left;">emerge -av layman (if you don't have it already)
 layman -a sunrise
 emerge democracy 
