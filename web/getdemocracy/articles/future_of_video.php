bb020aab701a64cc1455cba405c3395ec54bfc9f
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-14 20:23:06 +0000

    nope
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3251 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/articles/future_of_video.php
+++ web/getdemocracy/articles/future_of_video.php
@@ -23,29 +23,29 @@
 
 <p><strong>QUESTION 2:</strong> Will internet video be centralized in huge services like YouTube or Google Video, or will it be more broadly distributed (like blogs and web pages are), with huge (youtube), big (blip), medium (rocketboom), and small (average video bloggers on their own site) players?</p>
 
-<p>These are not entirely distinct questions, and that's a central message of this essay.  If video online is mostly web-based (question 1), the biggest centralized services have huge advantages (question 2).  If centralized services win, many of the wonderful things that come from TV meeting the internet will evaporate. </p>
+<p>These are not entirely distinct questions, and that's a central message of this essay.  If video online is mostly web-based (question 1), the biggest centralized services have huge advantages (question 2).  If centralized services win, many of the wonderful things that can come from TV meeting the internet will evaporate. </p>
 
 <h3>2: Are We Moving in the Wrong Direction?</h3>
 
-<p>So far, the answers to both of these questions have gone mostly in what I believe is the 'wrong' direction: towards 2 huge centralized services (YouTube and Google Video) and towards browser-based viewing.  </p>
+<p>So far, the answers to both of these questions have gone mostly in what I believe is the 'wrong' direction: towards two huge centralized services (YouTube and Google Video) and towards browser-based viewing.</p>
 
-<p>That's not to say that video RSS isn't doing well. It is. We've seen the number of channels in our Channel Guide increase 6 fold in the past year and channels are getting created faster and faster.  But there's big and then there's BIG.  YouTube is gigantic in a way that video RSS doesn't approach right now.</p>
+<p>That's not to say that video RSS isn't doing well. It is. We've seen the number of channels in the Democracy Channel Guide increase 6 fold in the past year and channels are getting created faster and faster every day.  But there's big and then there's BIG.  YouTube is gigantic in a way that video RSS doesn't approach right now.</p>
 
-<p>YouTube doesn't need defending, but I want to to be clear about exactly where more concerns are focused.  As a service and a universe, YouTube is amazing.  It has become a visual search engine of human experience.  So far, YouTube has done more to democratize video online than my organization or any of the other companies and advocates that are working for open-access and open-standards.  </p>
+<p>YouTube doesn't need defending, but I want to to be clear about exactly where my concerns are focused.  As a service and a universe, YouTube is amazing.  It has become a visual search engine of human experiences.  So far, YouTube has done more to democratize video online than my organization or any of the companies, organizations, and advocates that are working for open-access and open-standards.</p>
 
-<p>But --and this is crucial-- YouTube is spending money like crazy.  They'll need to make it all back sometime and I'm nervous about how they will do it.  Do YouTube executives have any option other than to hold viewers and creators hostage to ads on videos?  Maybe not. </p>
+<p>But --and this is crucial-- YouTube is spending money like crazy.  At some point, they'll need to make it all back. I'm nervous about how they will do that.  Do YouTube executives have any option other than to hold viewers and creators hostage to ads on videos?</p>
 
-<p>Furthermore, I worry that the near-monopoly strength of YouTube's network effect is dragging along people who don't actually want to use the service-- "If my video isn't on YouTube, will anyone find it??"  MySpace is the best example of this un-resistable social force in action-- how many web 2.0 gurus cringe everyday when they try to update their MySpace page? MySpace, by the way, is trying to be the next YouTube (that's the real dystopic scenario here).</p>
+<p>Furthermore, I worry that the near-monopoly strength of YouTube's network effect is dragging along people who don't actually want to use the service-- "If my video isn't on YouTube, how will anyone find it??"  In the social networking domain, MySpace is the best example of this un-resistable social pressure in action-- how many web 2.0 gurus cringe everyday when they try to login and update their MySpace page? MySpace, by the way, is trying to be the next YouTube (if you're looking for a real dystopia for online video, it's that).</p>
 
-<p>That network effect doesn't just endanger creators and viewers, it also will stifle competition.  Smaller video services that don't have YouTube's network effect will become backwaters.</p>
+<p>The network effect of online video services doesn't just endanger creators and viewers, it also stifles competition.  Smaller web video services that don't have YouTube's network effect will become backwaters.</p>
 
 <h3>3: Put Viewers at the Center, Not Companies</h3>
 
-<p>How do you avoid a world where YouTube is the arbiter of all video content?  You do it by centering the video experience around viewers rather than around big services.  That's not what the venture capitalists want, but blogs aren't what they wanted either (they wanted web portals).</p>
+<p>How do you avoid a world where YouTube is the arbiter of all video content?  You do it by centering the video experience around viewers rather than around video hosting companies.  That's not what the venture capitalists want, but blogs aren't what they wanted either (they wanted web portals).</p>
 
-<p>Putting viewers at the center means that everyone who wants to watch video has a homebase where they can access or pull together what they want to watch from any website or any service.  For miscellaneous videos, this means a search engine that gives results from any service and let's you watch what you find without jumping around from site to site.  </p>
+<p>Putting viewers at the center means giving everyone who wants to watch video a homebase where they can access videos from any hosting service or website.  For miscellaneous videos, like the ones that have made YouTube so popular, this means a search engine that gives results from any service and let's you watch what you find without jumping around from site to site.  </p>
 
-<p>For more serious videos-- things that are produced from a known source on a regular basis, like a TV show-- the homebase for video in an RSS aggregator.  The can be a desktop application (like the one we make) or a web-based aggregator.  The important is that viewers can pull together video from anywhere on the web.</p>
+<p>For more serious videos-- stuff that's produced by known creators on a regular basis (like a daily or weekly show)-- the best homebase is an RSS aggregator.  The can be a desktop application (like the one we make) or a web-based aggregator.  The important thing is that viewers can pull together video from anywhere on the web.</p>
 
 <p>The separation of publishing services and viewing services is crucial: viewers could care less where the video is hosted, as long as they can watch it.  A separation leaves publishers free to choose the hosting service that fits them best.  If they like the elegant user interfaces of Blip or Vimeo, they go there. If they like the opportunity to earn money with Revver, they go there.  If they want to offer ultra-high resolution video, they might host torrents on MoveDigital.  Or maybe they want to control their videos even more closely and host them on their own site (as Rocketboom appears to do).  With RSS, publishers are in control.</p>
 
