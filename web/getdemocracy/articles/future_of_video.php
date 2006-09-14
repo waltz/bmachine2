2262677a750198f98c67d3e6c4de030427991f2a
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-14 18:22:22 +0000

    article
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3198 bce20531-a64c-4166-8316-56baf4a01729

---
+++ web/getdemocracy/articles/future_of_video.php
@@ -0,0 +1,107 @@
+<?php include "../include/start.php"; ?>
+		
+		<div id="content-2col">
+			<div id="content-left-2col">
+			<div id="sub-main-2col">
+				<h2>Why Video Needs to Work Like the Rest of the Internet</h2>
+				<h3><span class="byline">by Nicholas Reville</span></h3>
+				
+
+<p>I want to convince you that if you care about open standards, open source, deep linking, and all the other things that make the internet wonderful, you should care about where video online is headed.  </p>
+
+<p>We are living at a moment of massive media convergence onto the internet.  Will television move online in an open and accessible way?  Or will it remain centralized, controlled by a few large corporations?  So far, things aren't looking good.</p>
+
+<h2>Part I: The Two Key Questions</h2>
+
+<p>This essay started as an email to Mike Hudack at Blip.tv.  Blip is an excellent video blogging / video hosting service. My goal was to convince Mike that Blip should feature RSS subscribe buttons more prominently on their site and explicitly encourage their viewers to obtain content through RSS.  I would really love it if the would promote watching RSS with Democracy Player in particular, but any support for video RSS is good.  Good for the my organization (Participatory Culture Foundation) and, I believe, good for the future of media.  </p>
+
+<p>As I was writing to Mike, I started to think that I should turn the letter into a public essay, and try to convince you all.  So here it is.</p>
+
+<p>I started the email with what I see as the two key questions about the future of online video:</p>
+
+<p>QUESTION 1: Will internet video viewing be primarily web-page based or will it be primarily RSS based?</p>
+
+<p>QUESTION 2: Will internet video be centralized in huge services like YouTube or Google Video, or will it be more broadly distributed (like blogs and web pages are), with lots of huge (youtube), big (blip), medium (rocketboom), and small (average video bloggers on their own site) players?</p>
+
+<p>These are not entirely distinct questions, and that's a central message of this essay: if video online is mostly web-based (question 1), the biggest centralized services have huge advantages (question 2).  If that happens, many of the wonderful things that come from TV meeting the internet will evaporate. </p>
+
+<h2>Part II: Are We Moving in the Wrong Direction?</h2>
+
+<p>So far, the answers to both of these questions have gone mostly in what I believe is the 'wrong' direction: towards 2 huge centralized services (YouTube and Google Video) and towards browser-based viewing.  </p>
+
+<p>That's not to say that video RSS isn't doing well. It is. We've seen the number of channels in our Channel Guide increase 6 fold in the past year and channels are getting created faster and faster.  But there's big and then there's BIG.  YouTube is gigantic in a way that video RSS doesn't approach right now.</p>
+
+<p>YouTube doesn't need defending, but I want to to be clear about exactly where more concerns are focused.  As a service and a universe, YouTube is amazing.  It has become a visual search engine of human experience.  So far, YouTube has done more to democratize video online than my organization or any of the other companies and advocates that are working for open-access and open-standards.  </p>
+
+<p>But --and this is crucial-- YouTube is spending money like crazy.  They'll need to make it all back sometime and I'm nervous about how they will do it.  Do YouTube executives have any option other than to hold viewers and creators hostage to ads on videos?  Maybe not. </p>
+
+<p>Furthermore, I worry that the near-monopoly strength of YouTube's network effect is dragging along people who don't actually want to use the service-- "If my video isn't on YouTube, will anyone find it??"  MySpace is the best example of this un-resistable social force in action-- how many web 2.0 gurus cringe everyday when they try to update their MySpace page? MySpace, by the way, is trying to be the next YouTube (that's the real dystopic scenario here).</p>
+
+<p>That network effect doesn't just endanger creators and viewers, it also will stifle competition.  Smaller video services that don't have YouTube's network effect will become backwaters.</p>
+
+<h2>Part III: Put Viewers at the Center, Not Companies</h2>
+
+<p>How do you avoid a world where YouTube is the arbiter of all video content?  You do it by centering the video experience around viewers rather than around big services.  That's not what the venture capitalists want, but blogs aren't what they wanted either (they wanted web portals).</p>
+
+<p>Putting viewers at the center means that everyone who wants to watch video has a homebase where they can access or pull together what they want to watch from any website or any service.  For miscellaneous videos, this means a search engine that gives results from any service and let's you watch what you find without jumping around from site to site.  </p>
+
+<p>For more serious videos-- things that are produced from a known source on a regular basis, like a TV show-- the homebase for video in an RSS aggregator.  The can be a desktop application (like the one we make) or a web-based aggregator.  The important part is that viewers can pull together video from anywhere on the web.</p>
+
+<p>The separation of publishing services and viewing services is crucial: viewers could care less where the video is hosted, as long as they can watch it.  A separation leaves publishers free to choose the hosting service that fits them best.  If they like the elegant user interfaces of Blip or Vimeo, they go there. If they like the opportunity to earn money with Revver, they go there.  If they want to offer ultra-high resolution video, they might host torrents on MoveDigital.  Or maybe they want to control their videos even more closely and host them on their own site (as Rocketboom appears to do).  With RSS, publishers are in control.</p>
+
+<p>All that said, I understand why services like Blip have been reluctant to push feeds.  Websites are the bread and butter of any internet company and page views are next to godliness. Promoting your RSS feeds is counter-intuitive-- why would you encourage users to leave your site?  Because small services can't and won't beat YouTube and Google and MySpace at the web game.  Those companies are too big, too well funded, and have hired too many talented people that will continue to improve their service.  Video RSS lets hosting services innovate on behalf of publishers, competing for their business. </p>
+
+<p>And really, it's not either / or.  Smaller hosting services and individual video creators can start pushing video RSS while still having a great user experience on the web.  </p>
+
+<h2>Part IV: How You Can Make the Future of Television Open and Awesome</h2>
+
+<p>If you cringe at the thought of online video becoming a 'walled garden' like MySpace is or dial-up AOL was, you can change it.</p>
+
+<p>Viewers: try a video RSS application like Democracy Player or FireANT.  Both have BitTorrent support, can show high-resolution video, and have built-in video search.  This isn't just good medicine, it's honestly the best online video experience you can find.</p>
+
+<p>Creators: no matter where you host your content, encourage your users to subscribe to your video RSS feed.  That way, they don't have to remember to check your website, they'll get your stuff delivered right to their desktop-- you've got them for good.  Serious video creators need rss because it lets them connect directly with their audience.</p>
+
+<p>Hosting Companies: create RSS feeds for everything (users, tags, popular videos, etc).  Put RSS subscribe buttons prominently on every page and explain to your viewers what it means and why they would enjoy watching with a video application.</p>
+
+<p>Advocates: video online has had a 2nd rate reputation with the tech elite.  It seems a little trashy and has tended to appeal to the lowest common denominator.  But that's changing.  Truly excellent video channels are popping-up.  Don't forget that television is the most important mass medium in our culture.  So when you talk about open-source, open-standards, Firefox, net-neutrality, xhtml/css, blogosphere and netroots, you should also be talking about video over RSS.  It's how the internet is supposed to work.</p>
+
+
+
+			</div>
+			<div id="content-right-2col-art">
+				<ul class="sidebar-nav-art">
+					<li><strong>The Key Points</strong>
+						<ul>
+							<li><br /><strong>FOR MAC USERS</strong><br />Democracy Player gives you fullscreen playback with beautiful playback controls, so you don't have to buy Quicktime Pro.</li>				
+							<li><strong>FOR WINDOWS USERS</strong><br />Democracy Player plays many more file formats that iTunes.</a></li>
+							<li><strong>FOR LINUX USERS</strong><br />You can't run iTunes on Linux, but you can run Democracy Player.</li>
+
+							<li><strong>FOR ALL USERS</strong>
+							<br />
+							Democracy Player was built specifically for video RSS.  The interface is easier to use and more intuitive. 
+<br /><br />
+							Democracy Player supports BitTorrent feeds and works as a standalone BitTorrent client.  iTunes does neither.
+<br /><br />
+							iTunes lets you sync to video iPods, Democracy Player does not (coming soon, we hope).
+<br /><br />
+							Like a TiVo, Democracy Player helps you manage disk space automatically.
+<br /><br />
+
+							Democracy Player's built-in Channel Guide lists more than 600 video channels, compared to only about 170 in iTunes' video podcast directory.  Democracy Player can play any feed that works with iTunes.  Most feeds that are in the iTunes directory are also in the Democracy Channel Guide.</li>
+						</ul>
+					</li>	
+				</ul>
+				<div id="lower-sidebar">
+				<h6>Convinced yet?</h6>
+				<strong>If so, add Democracy Player to your arsenal.</strong><br />
+				<h6><a style="font-size: 17px; color: #090;" href="<?= $base ?>/downloads/">Download Democracy Player</a></h6>
+				<span style="color: #999; font-size: 11px;">Free and Open Source<br />for Windows, Mac, and Linux</span>
+				<br />				<br />
+				</div>
+							
+				</div>
+			</div>
+			<div class="clearer"></div>
+		</div>
+
+<?php include "../include/end.php"; ?>
