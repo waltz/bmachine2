92ae2945fc781785bf9451f9515d5f36c653e96f
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-23 07:11:00 +0000

    tell a friend
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3370 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/include/email/press.php
+++ web/getdemocracy/include/email/press.php
@@ -1,7 +1,7 @@
 <h6>Get Press Updates</h6>
 <p>Occasional press releases and major updates. 
 <form action="http://participatoryculture.org/lists/?p=subscribe&id=15" method="post" name="subscribeform">
-	<input type="text" class="emailbox" name="email" value="email address" size="10" onfocus='this.value=""' />
+	<input type="text" class="emailbox" name="email" value="email address" size="15" onfocus='this.value=""' />
 	<input type=hidden name="makeconfirmed" value="1"/>
 	<input type="hidden" name="htmlemail" value="1" />
 	<input type="hidden" name="list[15]" value="signup" />
--- web/getdemocracy/include/email/publishers.php
+++ web/getdemocracy/include/email/publishers.php
@@ -2,7 +2,7 @@
 <p>Updates about how to get more viewers.
 <form action="http://participatoryculture.org/lists/?p=subscribe&id=2" 
 method="post" name="subscribeform">
-        <input type="text" class="emailbox" name="email" value="email address" size="10" onfocus='this.value=""' />
+        <input type="text" class="emailbox" name="email" value="email address" size="15" onfocus='this.value=""' />
         <input type=hidden name="makeconfirmed" value="1"/>
         <input type="hidden" name="htmlemail" value="1" />
         <input type="hidden" name="list[2]" value="signup" />
