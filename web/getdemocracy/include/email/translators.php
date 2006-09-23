cc136b27ce5cf3c4e1cf0da4ebf5c7e2981d8fc9
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-23 07:11:32 +0000

    tell a friend
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3371 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/include/email/translators.php
+++ web/getdemocracy/include/email/translators.php
@@ -1,3 +1,3 @@
 <h6>Translators</h6>
-<p>Democracy Player supports multiple languages-- we need your help! <br /><a href="https://launchpad.net/products/democracy/trunk/+pots/democracyplayer">Join the translation effort.</a>
+<p>Democracy is translated into 30 languages-- we need your help! <br /><a href="https://launchpad.net/products/democracy/trunk/+pots/democracyplayer">Join the translation effort.</a>
 </p>
