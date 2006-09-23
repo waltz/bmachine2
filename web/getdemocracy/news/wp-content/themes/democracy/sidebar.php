bc62445f85c0675fb23a31d21692e3d8e6504ee6
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-23 07:20:40 +0000

    tell a friend
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3383 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/news/wp-content/themes/democracy/sidebar.php
+++ web/getdemocracy/news/wp-content/themes/democracy/sidebar.php
@@ -10,11 +10,11 @@
 <h2>Articles</h2>
 
 <p style="padding-top: 5px;">
-Future of Online Video<br /><a href="http://www.getdemocracy.com/articles/future_of_video.php">Openness Matters. RSS Can Help.</a>
+<strong>Future of Online Video</strong>br /><a href="http://www.getdemocracy.com/articles/future_of_video.php">Openness Matters. RSS Can Help.</a>
 </p>
 
 <p>
-Video Podcast Shootout<br /><a href="http://www.getdemocracy.com/articles/video_podcast_shootout.php">Democracy vs. iTunes</a><br />
+<strong>Video Podcast Shootout</strong><br /><a href="http://www.getdemocracy.com/articles/video_podcast_shootout.php">Democracy vs. iTunes</a><br />
 </p>
 
 			<h2>Older Posts</h2>
