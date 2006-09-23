e48c16f0eff069dba7f5728f8f241fe6e09faaf6
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-23 07:09:16 +0000

    tell a friend
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3369 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/include/stayuptodate.php
+++ web/getdemocracy/include/stayuptodate.php
@@ -1,5 +1,5 @@
-<h6>Stay up to Date</h6>
-<p>Things are moving fast. Check out our blog and get email updates when we have big announcements. (We won't share your email address with anyone.)</p>
+<h6>Stay Posted</h6>
+<p>Because things are moving fast. We won't share your email address with anyone.</p>
 <?php include("email/viewers.php"); ?>
 <?php include("email/publishers.php"); ?>
 <?php include("email/translators.php"); ?>
