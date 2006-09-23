62b4b50547055af7b2e2a6c875d74210022b3563
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-23 05:57:51 +0000

    tell a friend
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3355 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/tellafriend/thanks.php
+++ web/getdemocracy/tellafriend/thanks.php
@@ -9,8 +9,8 @@
 					
 		<p>Thank you so much for telling a friend, it really makes a big difference for our efforts.  If you want to tell more friends, click the back button and change the email addresses.</p>
 		
-		<p><a href="/fight/">More ways you can help spread Democracy</a></p>
-
+		<p><a href="/fight/">More ways you can help spread Democracy >></a></p>
+<br /><br />
 	</div>		
 
 <?php include "../include/end.php"; ?>
