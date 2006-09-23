c5a56d50cedd5c4caaebdaa48ef0a0c7397118d5
npr <npr@bce20531-a64c-4166-8316-56baf4a01729>
2006-09-23 07:06:23 +0000

    tell a friend
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3367 bce20531-a64c-4166-8316-56baf4a01729

--- web/getdemocracy/include/stayuptodate.php
+++ web/getdemocracy/include/stayuptodate.php
@@ -1,5 +1,5 @@
 <h6>Stay up to Date</h6>
-<p>Things are moving fast. Check out our blog and signup for email notifications below (we won't share your email address with anyone and we only send occasional, important announcements).</p>
+<p>Things are moving fast. Check out our blog and get email updates when we have big announcements. (We won't share your email address with anyone.)</p>
 <?php include("email/viewers.php"); ?>
 <?php include("email/publishers.php"); ?>
 <?php include("email/translators.php"); ?>
--- web/getdemocracy/news/wp-content/themes/democracy/sidebar.php
+++ web/getdemocracy/news/wp-content/themes/democracy/sidebar.php
@@ -8,10 +8,14 @@
 
 
 <h2>Articles</h2>
-The future of video online: <a href="http://www.getdemocracy.com/articles/future_of_video.php">Openness Matters. RSS Can Help.</a>
-<br />
-Video podcast shootout: <a href="http://www.getdemocracy.com/articles/video_podcast_shootout.php">Democracy vs. iTunes</a><br />
 
+<ul>
+<li>
+The future of video online: <a href="http://www.getdemocracy.com/articles/future_of_video.php">Openness Matters. RSS Can Help.</a>
+</li>
+<li>
+Video podcast shootout: <a href="http://www.getdemocracy.com/articles/video_podcast_shootout.php">Democracy vs. iTunes</a><br />
+</li>
 
 			<h2>Older Posts</h2>
 				<ul>
