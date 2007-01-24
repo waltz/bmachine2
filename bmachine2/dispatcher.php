b1fe79be8a5732467200b93ff92a280dc95416f2
cbryan <cbryan@bce20531-a64c-4166-8316-56baf4a01729>
2007-01-24 02:35:18 +0000

    Added index.php and dispatcher.php skeletons.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4008 bce20531-a64c-4166-8316-56baf4a01729

---
+++ bmachine2/dispatcher.php
@@ -0,0 +1,13 @@
+<?php
+
+// Grab the parameters
+$param_1 = $_GET[1];
+$param_2 = $_GET[2];
+$param_3 = $_GET[3];
+
+// If the first parameter is a tag, delegate to the tag controller.
+
+// Instantiate singleton classes.
+
+
+?>
---
+++ bmachine2/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Simple, just includes the dispatcher.
+include(dispatcher.php);
+
+?>
