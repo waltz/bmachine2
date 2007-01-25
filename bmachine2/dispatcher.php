1c099f41bddd5539d88d31179fbb52d05e132cef
cbryan <cbryan@bce20531-a64c-4166-8316-56baf4a01729>
2007-01-25 03:28:49 +0000

    Preliminary dispatcher in place, wrote unit test skeleton.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4024 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine2/dispatcher.php
+++ bmachine2/dispatcher.php
@@ -1,13 +1,45 @@
 <?php
 
-// Grab the parameters
-$param_1 = $_GET[1];
-$param_2 = $_GET[2];
-$param_3 = $_GET[3];
+// Include the controllers
+require_once "channel_cntrl.php"
+require_once "video_cntrl.php"
+require_once "tag_cntrl.php"
 
-// If the first parameter is a tag, delegate to the tag controller.
+// Grab the parameters.
+var get_parameters = $_GET[1];
 
-// Instantiate singleton classes.
+// Parse out the parameters.
+var param_1 = strtok($parameters, "/");
+var param_2 = strtok("/");
+var param_3 = strtok("/");
 
+// If we only get the first parameter and no second parameter,
+// we should assume that it's a channel name.
+if(isset(param_1) && !isset(param_2)){
+	new channel_cntrl(param_1);
+}
+
+// Call the video controller if we get a channel and a video.
+if(isset(param_1) && isset(param_2)){
+	new video_cntrl(param_2);
+}
+
+// If the first parameter is a video then the second is the name
+// of the video.
+else if((param_1 == "video") && isset(param_2)){
+	new video_cntrl(param_2);
+}
+
+// If the first parameter is "tag" then the second must be the
+// user specified tag.
+else if((param_1 == "tag") && isset(param_2)){
+	new tag_cntrl(param_2);
+}
+
+// If no parameters were sent, go to the default channel.
+// This is also the catch-all if something goes wrong.
+else(!isset(get_parameters)){
+	new channel_cntrl();
+}
 
 ?>
---
+++ bmachine2/unit_tests/DispatcherTest.php
@@ -0,0 +1,14 @@
+<?php
+
+// Indespensible: http://www.phpunit.de/pocket_guide/3.1/en/index.html
+
+require_once 'PHPUnit/Framework.php';
+require_once '../dispatcher.php';
+
+class DispatcherTest extends PHPUnit_Framework_Testcase{
+
+	// Nothing to test yet... Gimme some controllers!
+	
+}
+
+?>
