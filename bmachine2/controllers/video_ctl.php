04e497b5a24288e1554ae5defa22fa2c6e0b42d9
cbryan <cbryan@bce20531-a64c-4166-8316-56baf4a01729>
2007-01-26 17:47:25 +0000

    Imported Smarty with modified settings.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4042 bce20531-a64c-4166-8316-56baf4a01729

---
+++ bmachine2/controllers/video_ctl.php
@@ -0,0 +1,39 @@
+<?php
+
+require '../smarty/Smarty.class.php';
+
+class VideoController
+{
+	// Constructor, retrieve function
+	function videoController()
+	{
+		
+	}
+	
+	// Create
+	function addVideo()
+	{
+		
+	}
+
+	// Retrieve
+	function retrieveVideo()
+	{
+		
+	}
+	
+	// Update
+	function updateVideo()
+	{
+		
+	}
+	
+	// Delete
+	function deleteVideo()
+	{
+		
+	}
+
+}
+
+?>
--- bmachine2/dispatcher.php
+++ bmachine2/dispatcher.php
@@ -1,45 +1,53 @@
 <?php
 
 // Include the controllers
-require_once "channel_cntrl.php"
-require_once "video_cntrl.php"
-require_once "tag_cntrl.php"
+require_once 'controllers/channel_ctl.php';
+require_once 'controllers/video_ctl.php';
+require_once 'controllers/tag_ctl.php';
 
 // Grab the parameters.
 var get_parameters = $_GET[1];
 
 // Parse out the parameters.
-var param_1 = strtok($parameters, "/");
-var param_2 = strtok("/");
-var param_3 = strtok("/");
-
-// If we only get the first parameter and no second parameter,
-// we should assume that it's a channel name.
-if(isset(param_1) && !isset(param_2)){
-	new channel_cntrl(param_1);
-}
-
-// Call the video controller if we get a channel and a video.
-if(isset(param_1) && isset(param_2)){
-	new video_cntrl(param_2);
-}
+var param_1 = strtok($parameters, '/');
+var param_2 = strtok('/');
+var param_3 = strtok('/');
 
 // If the first parameter is a video then the second is the name
 // of the video.
-else if((param_1 == "video") && isset(param_2)){
-	new video_cntrl(param_2);
+if(param_1 == 'video')
+{
+	new video_ctl(param_2);
 }
 
-// If the first parameter is "tag" then the second must be the
+// If the first parameter is 'tag' then the second must be the
 // user specified tag.
-else if((param_1 == "tag") && isset(param_2)){
-	new tag_cntrl(param_2);
+else if(param_1 == 'tag')
+{
+	new tag_ctl(param_2);
+}
+
+// Delegate to the user controller if the first param is 'user'.
+else if(param_1 == 'users')
+{
+	new users_ctl(param_2);
+}
+
+// Call the video controller if we get a channel and a video.
+else if(isset(param_1) && isset(param_2)){
+	new video_ctl(param_2);
+}
+
+// If we only get the first parameter and no second parameter,
+// we should assume that it's a channel name.
+else if(isset(param_1) && !isset(param_2)){
+	new channel_ctl(param_1);
 }
 
 // If no parameters were sent, go to the default channel.
 // This is also the catch-all if something goes wrong.
 else(!isset(get_parameters)){
-	new channel_cntrl();
+	new channel_ctl();
 }
 
 ?>
