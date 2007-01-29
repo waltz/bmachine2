346b25835297be4243f6ddafb1c5a2a232fbc89d
cbryan <cbryan@bce20531-a64c-4166-8316-56baf4a01729>
2007-01-29 19:14:22 +0000

    Imported new VideoController.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4056 bce20531-a64c-4166-8316-56baf4a01729

---
+++ bmachine2/controllers/VideoController.php
@@ -0,0 +1,39 @@
+<?php
+
+require '../smarty/Smarty.class.php';
+
+class VideoController
+{
+	// Constructor, retrieve function
+	function VideoController($param_2)
+	{
+		
+	}
+	
+	// Create
+	function addVideo()
+	{
+		
+	}
+
+	// Retrieve
+	function retrieveVideo()
+	{
+		
+	}
+	
+	// Update
+	function updateVideo()
+	{
+		
+	}
+	
+	// Delete
+	function deleteVideo()
+	{
+		
+	}
+
+}
+
+?>
