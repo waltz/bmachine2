c5beded760fcd4718a524c9e5fc24a706ac81d23
drew <drew@bce20531-a64c-4166-8316-56baf4a01729>
2007-02-05 20:28:04 +0000

    fixing smarty
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4142 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine2/index.php
+++ bmachine2/index.php
@@ -1,6 +1,32 @@
 <?php
 
 // Simple, just includes the dispatcher.
-include('dispatcher.php');
+//include('dispatcher.php');
+
+//set absolute server path. this is the current directory
+$path = getcwd();
+//echo 'current path: ' . $path;
+
+
+//Setup smarty
+
+//include the smarty library
+require($path . '/smarty/Smarty.class.php');
+
+//create a new instance
+$smarty = new Smarty();
+
+//set up all the correct paths
+$smarty->template_dir = $path . '/themes/';
+$smarty->compile_dir = $path . '/smarty/templates_c/';
+$smarty->cache_dir = $path . '/smarty/cache';
+$smarty->config_dir = $path . '/smarty/configs';
+
+//assign any smarty variables and function
+$smarty->assign('name', 'Ned');
+
+//Display template
+$smarty->display('a.tpl');
+
 
 ?>
