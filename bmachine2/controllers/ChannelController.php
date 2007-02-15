ec9d1be960053512c376b2f7e6bdf36b09874a5a
drew <drew@bce20531-a64c-4166-8316-56baf4a01729>
2007-02-15 21:18:40 +0000

    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4248 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine2/controllers/ChannelController.php
+++ bmachine2/controllers/ChannelController.php
@@ -6,31 +6,31 @@ class ChannelController
 	// Constructor, retrieve function
 	function ChannelController($param_2)
 	{
-		
+		echo 'hello';
 	}
 	
 	// Create
 	function addChannel()
 	{
-		
+		echo 'hello';		
 	}
 
 	// Retrieve
 	function viewChannel()
 	{
-		
+		echo 'hello';		
 	}
 	
 	// Update
 	function editChannel()
 	{
-		
+		echo 'hello';		
 	}
 	
 	// Delete
 	function removeChannel($id)
 	{
-		$delete_query = "DELETE FROM channels WHERE id=\'$id\';";
+		//$delete_query = "DELETE FROM channels WHERE id=\'$id\'\;";
 	}
 
 }
