aa339774313dc1fd6ee91b578c36cce62b558f65
drew <drew@bce20531-a64c-4166-8316-56baf4a01729>
2007-02-16 03:35:49 +0000

    its not going to work, Im telling you...
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4263 bce20531-a64c-4166-8316-56baf4a01729

--- bmachine2/dispatcher.php
+++ bmachine2/dispatcher.php
@@ -28,6 +28,8 @@ $param_1 = strtok($parameters, '/');
 $param_2 = strtok('/');
 $param_3 = strtok('/');
 
+new FrontPageController();
+
 //Instantiate DatabaseController
 $db = new DatabaseController();
 
